f5abecaed3f75c506e284deca5b74a28269141e9
Valentina <valentinasedelkova@yandex.ru>
2023-07-29 21:00:49 +0300

    added all tasks

---
+++ Task50/Program.cs
@@ -0,0 +1,57 @@
+﻿// // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int [,] massive = new int [3, 7];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 7; j++)
+    {
+        massive[i, j] = new Random().Next(0, 101); 
+    }
+}
+
+void PrintDoubleMassive(int[,] massive)
+{
+    for(int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+string NumberElement (int[,] array, int row, int column)
+{
+    int rowCount = array.GetLength(0);
+    int columnCount = array.GetLength(1);
+    if (row >= 0 && row < rowCount && column >= 0 && column < columnCount)
+    {
+        return array[row -1, column-1].ToString();
+    }
+    else{
+        return "Такого элемента нет";
+    }
+}
+
+PrintDoubleMassive(massive);
+int row = GetInfo("Введите координату строки массива: ");
+int column = GetInfo("Введите координату столбца массива: ");
+
+
+string numberElement = NumberElement(massive , row, column); 
+Console.WriteLine(numberElement);
---
+++ Task52/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] massive = new int [3, 3];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        massive[i, j] = new Random().Next(0, 101); 
+    }
+}
+
+void PrintDoubleMassive(int[,] massive)
+{
+    for(int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+void AverageColumn(int[,] array)
+{
+    int rowCount = array.GetLength(0);
+    int colCount = array.GetLength(1);
+    int [] columnSum = new int[colCount];
+    for(int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            columnSum[j] += array[i, j];
+        }
+    }
+        double[] columnAverage = new double[colCount];
+        for (int j = 0; j < colCount; j++) 
+        {
+            columnAverage[j] = Math.Round(((double)columnSum[j] / rowCount), 2);
+        }
+Console.WriteLine("Среднее арифметическое элементов в каждом столбце:");
+        for (int k = 0; k < colCount; k++)
+        {
+            Console.WriteLine($"Столбец {k + 1}: {columnAverage[k]}");
+        }    
+}
+
+PrintDoubleMassive(massive);
+AverageColumn(massive);
